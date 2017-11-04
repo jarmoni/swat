c5e298f15f309853e12dd66d9f12c9e78c2b9df1
michas <michas@jarmoni.org>
2017-11-04 15:02:24 +0100

    added images + buttons

--- src/main/java/org/jarmoni/ws_ssh_terminal/Application.java
+++ src/main/java/org/jarmoni/ws_ssh_terminal/Application.java
@@ -1,23 +1,19 @@
 package org.jarmoni.ws_ssh_terminal;
 
-import org.jarmoni.ws_ssh_terminal.ws.message.PingMessage;
-import org.jarmoni.ws_ssh_terminal.ws.message.WsMessageWrapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.google.gson.Gson;
 
 // Read:
 // https://auth0.com/blog/implementing-jwt-authentication-on-spring-boot/
 // http://blog.florian-hopf.de/2017/08/spring-security.html
 // https://blog.jdriven.com/2014/10/stateless-spring-security-part-2-stateless-authentication/
+// https://stackoverflow.com/questions/16777003/what-is-the-easiest-way-to-disable-enable-buttons-and-links-jquery-bootstrap
+// https://www.freefavicon.com
 @SpringBootApplication
 public class Application {
 
 	public static void main(final String[] args) {
-		final WsMessageWrapper wrapper = new WsMessageWrapper();
-		wrapper.ping = new PingMessage("123");
-		System.out.println(new Gson().toJson(wrapper));
+
 		SpringApplication.run(Application.class, args);
 	}
 }
